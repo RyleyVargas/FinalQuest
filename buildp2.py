f2399982caa8fbed8f0af755a08e966c2b1a4c68
RyleyVargas <62559765+RyleyVargas@users.noreply.github.com>
2020-05-20 13:40:06 -0700

    Update buildp2.py

--- buildp2.py
+++ buildp2.py
@@ -1,21 +1,24 @@
 # This File was created  by Ryley Vargas
 # Sources (how to import photos into py.game): https://pythonprogramming.net/displaying-images-pygame/
-
-'''
-
-'''
+# source how to add graphics https://kidscancode.org/blog/2016/08/pygame_shmup_part_4/
+# where i got my the images for my ships https://opengameart.org/
+# Mr.Cozort's code from apprentice group
 
 import pygame as pg
 from pygame.sprite import Sprite
 import random
 from os import path
 from pathlib import Path
+
+#creates path for image
 img_folder = Path("img")
 bg_img = img_folder / "bg.png"
 print(bg_img)
 
+#game directory: allows to pull to img folder
 game_dir = path.join(path.dirname(__file__))
 print(game_dir)
+
 
 # global variables
 WIDTH = 480
@@ -35,23 +38,32 @@ YELLOW = (255, 255, 0)
 pg.init()
 pg.mixer.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Shmup!")
+#game title
+pg.display.set_caption("Space Invasion")
 clock = pg.time.Clock()
 
-# Load all game graphics
-background = pg.image.load(path.join(game_dir + "/img/bg.png")).convert()
+#My changes:
+# I changed the backrground 
+# wanted a more detailed backround because the space backgrounds on the other ones looked too plain
+# all of my images came from opengameart: https://opengameart.org/
+# where got the idea to use the art from opengameart https://www.youtube.com/watch?v=mOckdKp3V38&feature=emb_logo
+# Game graphics:
+background = pg.image.load(path.join(game_dir + "/img/background.png")).convert()
 background_rect = background.get_rect()
 background_rect2 = background.get_rect()
-player_img = pg.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+# the ships are more detailed, and a little less cartoonish
+player_img = pg.image.load(path.join(img_dir, "player_ship.png")).convert()
 mob_img = pg.image.load(path.join(game_dir + "/img/mob.png")).convert()
+#lazer and spit are similar, but different colors
 lazer_img = pg.image.load(path.join(game_dir + "/img/lazer.png")).convert()
 spit_img = pg.image.load(path.join(game_dir + "/img/spit.png")).convert()
 powerup_images = {}
+# i changed the power up to be a purple orb because it seemed more fitting
 powerup_images['shield'] = pg.image.load(path.join(game_dir + "/img/power.png")).convert()
 powerup_images['gun'] = pg.image.load(path.join(game_dir + "/img/power.png")).convert()
 player_mini_img = pg.transform.scale(player_img, (25, 19))
 
-
+#sets font
 font_name = pg.font.match_font('arial')
 def draw_text(surf, text, size, x, y):
     font = pg.font.Font(font_name, size)
@@ -60,15 +72,19 @@ def draw_text(surf, text, size, x, y):
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
 
+#draws health
 def draw__health(surf, x, y, w):
     outline_rect = pg.Rect(x, y, 100, 20)
     fill_rect = pg.Rect(x, y, w, 20)
+    #fills white and red
     pg.draw.rect(surf, RED, fill_rect)
     pg.draw.rect(surf, WHITE, outline_rect, 2)
 
+#the health bar    
 def draw_shield_bar(surf, x, y, pct):
     if pct < 0:
         pct = 0
+  #bar length and height
     BAR_LENGTH = 100
     BAR_HEIGHT = 10
     fill = (pct / 100) * BAR_LENGTH
@@ -77,6 +93,7 @@ def draw_shield_bar(surf, x, y, pct):
     pg.draw.rect(surf, GREEN, fill_rect)
     pg.draw.rect(surf, WHITE, outline_rect, 2)
 
+#draws life
 def draw_lives(surf, x, y, lives, img):
     for i in range(lives):
         img_rect = img.get_rect()
@@ -87,35 +104,30 @@ def draw_lives(surf, x, y, lives, img):
 class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image = pg.Surface((50,40))
-        self.image = pg.transform.scale(player_img, (50, 40))
+        # decreased the size of the player so you dont get hit as easily
+        # and i just like the look of it better in general
+        self.image = pg.transform.scale(player_img, (40, 30))
         self.image = player_img
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT -10
+        #movement pf the players ship
         self.speedx = 0
         self.speedy = 10
         self.power = 1
         self.shield = 100
-        self.lives = 10
+        #increased players lives so they game goes longer
+        self.lives = 12
     def update(self):
         self.speedx = 0
-        self.speedy = 0
         keystate = pg.key.get_pressed()
         if keystate[pg.K_a]:
             self.speedx = -8
         if keystate[pg.K_d]:
             self.speedx = 8
-        if keystate[pg.K_SPACE]:
-           self.pew()
-        if keystate[pg.K_w]:
-            self.speedy = -8
-        if keystate[pg.K_s]:
-            self.speedy = 8
         self.rect.x += self.speedx
-        self.rect.y += self.speedy
-    # taken and modified from Bradfield - power up method
+        # taken and modified from Bradfield - power up method
     def powerup(self):
         self.power += 1
         self.power_time = pg.time.get_ticks()
@@ -134,7 +146,6 @@ class Pow(Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = center
         self.speedy = 5
-
     def update(self):
         self.rect.y += self.speedy
         # kill if it moves off the top of the screen
@@ -149,16 +160,17 @@ class Mob(Sprite):
         self.image.set_colorkey(BLACK)
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
+        #speed of mob spawn
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
         self.rect.y = random.randrange(0, 250)
         self.speedy = random.randrange(1, 10)
         self.speedx = random.randrange(1, 8)
-        self.hitpoints = 10
+        #increase hitpoint, so player focuses on evading more 
+        self.hitpoints = 20
         def pew(self):
         spit = Spit(self.rect.centerx, self.rect.top)
         all_sprites.add(spit)
         spits.add(spit)
-        # print('trying to shoot..')
     def update(self):
         self.health_image = pg.Surface(self.hitpoints, 10)
         self.health_rect.x = self.x
@@ -166,7 +178,6 @@ class Mob(Sprite):
         self.rect.x += self.speedx
         if random.random() > 0.99:
             self.pew()
-        # self.rect.y += self.speedy
         if self.rect.x > WIDTH or self.rect.x < 0:
             self.speedx*=-1
             self.rect.y += random.randrange(5,25)
@@ -253,7 +264,7 @@ while running:
             pow = Pow(hit.rect.center)
             all_sprites.add(pow)
             powerups.add(pow)
-
+            
     if len(mobs) == 0:
         for i in range(8):
             m = Mob()
